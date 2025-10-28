bff4a4d6e3b0b23290117e880595a173a2abd8cb
IlliaProrokov <mc_iprorokov27@mcjvs2.org>
2025-10-28 09:07:55 -0400

    initial push

---
+++ js01/script.js
@@ -0,0 +1,7 @@
+// Comment: Author - [Illia Prorokov]
+
+// Use console.log() to show that the external file has loaded
+console.log('External file loaded!');
+
+// Use document.write() to add a paragraph
+document.write('<p>Today I learned how to link an external JavaScript file. It was interestingand little difficult</p>');
